a8cc77d08c2505486692f7652201a235d111c33c
AxelPranata <S10273701@connect.np.edu.sg>
2026-02-04 10:01:52 +0800

    Add loading of hawker centers from database functionality
    - Add hawker_centers.js to retreive and create hawker center items
    - Removed hard coded items in hawker_centers.html
    - Add hawker center images hawker_center images folder
    - Clicking from hawker center to food stalls functionality is still WIP

---
+++ assets/scripts/hawker_centers.js
@@ -0,0 +1,72 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  increment
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+/* =========================
+   Firebase config
+========================= */
+const firebaseConfig = {
+  apiKey: "AIzaSyDac46txTyLdtBlJ4gvcvl2yxTlduC_FUE",
+  authDomain: "hawkers-native.firebaseapp.com",
+  projectId: "hawkers-native",
+  storageBucket: "hawkers-native.firebasestorage.app",
+  messagingSenderId: "25256491882",
+  appId: "1:25256491882:web:99a54c487373e155278313"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function createItem(id, name, img) {
+  // Create card div
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.style.background = `
+    linear-gradient(to top right, rgba(3, 8, 31, 1), rgba(3, 8, 31, 0.8), rgba(3, 8, 31, 0.0), rgba(3, 8, 31, 0), rgba(3, 8, 31, 0)),
+    linear-gradient(to left, rgba(3, 8, 31, 1), rgba(3, 8, 31, 0.4), rgba(3, 8, 31, 0.2), rgba(3, 8, 31, 0), rgba(3, 8, 31, 0)),
+    url('${img}') no-repeat center center / cover`;
+
+  // Add content
+  const hawkerCenterid = document.createElement('p');
+  hawkerCenterid.classList.add('highlight');
+  hawkerCenterid.textContent = `#${id}`;
+
+  const hawkerCenterName = document.createElement('p');
+  hawkerCenterName.textContent = name;
+
+  // Append content
+  item.appendChild(hawkerCenterid);
+  item.appendChild(hawkerCenterName);
+
+  // Make card clickable (navigate with URL param)
+  item.addEventListener('click', () => {
+    window.location.href = `food_stalls.html?id=${id}`;
+  });
+
+  console.log(`Created ${id} ${name} ${img}`)
+
+  return item;
+}
+
+// Load cards from Firestore
+async function loadCards() {
+  const container = document.querySelector('.container');
+  const snapshot = await getDocs(collection(db, "hawker-centers"));
+
+  snapshot.forEach(doc => {
+    const data = doc.data();
+    const item = createItem(doc.id, data.name, data.imagePath);
+    container.appendChild(item);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    loadCards();
+})
+
