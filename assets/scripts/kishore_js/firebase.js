651ec801cea445e30397b97f1895f1bf710a7c95
kishore2007 <rajakumarkishore28@gmail.com>
2026-02-05 10:52:22 +0800

    adding firebase for me

---
+++ assets/scripts/kishore_js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDac46txTyLdtBlJ4gvcvl2yxTlduC_FUE",
+  authDomain: "hawkers-native.firebaseapp.com",
+  projectId: "hawkers-native",
+  storageBucket: "hawkers-native.firebasestorage.app",
+  messagingSenderId: "25256491882",
+  appId: "1:25256491882:web:99a54c487373e155278313"
+};
+
+// 🔥 Initialize Firebase ONCE
+const app = initializeApp(firebaseConfig);
+
+// 🔥 Export ONCE
+export const db = getFirestore(app);
+export const auth = getAuth(app);
